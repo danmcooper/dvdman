ad7c92da2c023f09f59f34283b1c7599cd804ac1
Dan Cooper <dan@trapit.com>
2012-10-02 23:16:34 -0700

    rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dvdman::Application.config.secret_token = '09bd2be8fc54f11083e37fc4f862e268653382c9f3692bb7e6bd7c73a4ee9af01cd2853d83a5a9e5abd86776dc039db0a325f79b168fad95eca3add83e97043c'
